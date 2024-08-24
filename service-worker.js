11b5a0cd1bdba67ccfdd02733b43c78a7c9e58ae
Muhammed Rabeeh K P <122214209+muhammed-rabeeh@users.noreply.github.com>
2024-08-24 10:28:07 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,45 @@
-self.addEventListener('install', event => {
-    console.log('Service worker installed:', event);
+const CACHE_NAME = 'pwa-cache-v1';
+const URLS_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/script.js',
+    '/manifest.json',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
+];
+
+// Install event
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(URLS_TO_CACHE);
+        })
+    );
 });
 
-self.addEventListener('activate', event => {
-    console.log('Service worker activated:', event);
+// Fetch event
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
 
-self.addEventListener('fetch', event => {
-    console.log('Fetch event:', event);
+// Activate event
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
 });
+
