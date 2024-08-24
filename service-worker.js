9e2855649df12077893ce40a6c5b86292a9ff70b
Muhammed Rabeeh K P <122214209+muhammed-rabeeh@users.noreply.github.com>
2024-08-24 10:24:08 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,44 +1,11 @@
-const CACHE_NAME = 'pwa-cache-v1';
-const URLS_TO_CACHE = [
-    './', // Adjust if this is the subdirectory
-    '/task_manager/index.html', // Adjust if this is the subdirectory
-    '/task_manager/styles.css', // Adjust if this is the subdirectory
-    '/task_manager/script.js', // Adjust if this is the subdirectory
-    '/task_manager/manifest.json', // Adjust if this is the subdirectory
-    '/task_manager/icons/icon-192x192.png',
-    '/task_manager/icons/icon-512x512.png'
-];
-
-// Install event
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(URLS_TO_CACHE);
-        })
-    );
+self.addEventListener('install', event => {
+    console.log('Service worker installed:', event);
 });
 
-// Fetch event
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener('activate', event => {
+    console.log('Service worker activated:', event);
 });
 
-// Activate event
-self.addEventListener('activate', (event) => {
-    const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => {
-                    if (!cacheWhitelist.includes(cacheName)) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
+self.addEventListener('fetch', event => {
+    console.log('Fetch event:', event);
 });
