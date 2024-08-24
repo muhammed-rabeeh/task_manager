3830783c7ce95205e3fa151d1a87d48debc2cfec
Muhammed Rabeeh K P <122214209+muhammed-rabeeh@users.noreply.github.com>
2024-08-24 12:20:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -352,7 +352,7 @@ function displayDaySummary() {
 // Register Service Worker
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js')
+        navigator.serviceWorker.register('/task_manager/service-worker.js')
             .then((registration) => {
                 console.log('Service Worker registered with scope:', registration.scope);
             })
