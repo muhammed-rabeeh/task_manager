a1e2157e180fb659dded80be82ced5370e3c6e4c
Muhammed Rabeeh K P <122214209+muhammed-rabeeh@users.noreply.github.com>
2024-08-24 09:38:18 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 const CACHE_NAME = 'pwa-cache-v1';
 const URLS_TO_CACHE = [
-    '/',
+    '/task_manager/',
     '/index.html',
     '/styles.css',
     '/script.js',
     '/manifest.json',
-    '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png'
+    '/task_manager/icons/icon-192x192.png',
+    '/task_manager/icons/icon-512x512.png'
 ];
 
 // Install event
